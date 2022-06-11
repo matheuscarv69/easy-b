97fd869c57b9e6c91a48e5d00ef72e67da6e0fee
matheuscarv69 <matheus9126@gmail.com9>
2022-06-11 09:41:18 -0300

    :tada: initial commit

---
+++ src/main/java/easyb/EasyBApplication.java
@@ -0,0 +1,13 @@
+package easyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/easyb/EasyBApplicationTests.java
@@ -0,0 +1,13 @@
+package easyb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
